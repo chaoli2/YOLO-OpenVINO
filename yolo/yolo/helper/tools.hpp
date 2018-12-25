e15be54e5db7a456008d56d214d7452a95c441d0
@intel <iamsunfy@yeah.net>
2018-12-25 16:21:19 +0800

    temp

--- yolo/yolo/helper/tools.hpp
+++ yolo/yolo/helper/tools.hpp
@@ -40,6 +40,13 @@ typedef struct detection{
 int entry_index(int IH, int IW, int location, int entry){
     int n =   location / (IW*IH);
     int loc = location % (IW*IH);
+    // printf("l.w: %i l.h: %i l.coords: %i loc: %i entry: %i return: %i\n",
+    //     IH,
+    //     IW,
+    //     4,
+    //     location,
+    //     entry,
+    //     n*IW*IH*(4+80+1) + entry*IW*IH + loc);
     return n*IW*IH*(4+80+1) + entry*IW*IH + loc;
 }
 
@@ -91,40 +98,175 @@ void correct_region_boxes(vector<detection>& dets, int n, int w, int h, int netw
 vector<detection> yoloNetParseOutput(const float* output_data, int IH, int IW){
     vector<detection> dets (IW*IH*5);
     float thresh = 0.2;
-    for(int i =0; i < IW*IH; i++){
+    for(int i =0; i < 19*19; i++){
         int row = i / IW;
         int col = i % IW;
         for(int n = 0; n < 5; n++){
             int index = n*IW*IH + i;
             int obj_index  = entry_index(IH, IW, n*IW*IH + i, 4);
             int box_index  = entry_index(IH, IW, n*IW*IH + i, 0);
-            // cout << "obj_index: " << obj_index
-            // << "\tbox_index: " << box_index << endl;
             float scale = output_data[obj_index];
             dets[index].bbox = get_region_box(output_data, n, box_index, col, row, IW, IH, IW*IH);
             dets[index].objectness = scale > thresh ? scale : 0;
+            if(dets[index].objectness)
+            printf("scale:%f [%f, %f]-[%f, %f] score:%f obj_idx:%i box_idx:%i\n", 
+                scale, 
+                dets[index].bbox.x, dets[index].bbox.y,
+                dets[index].bbox.w, dets[index].bbox.h,
+                dets[index].objectness,
+                obj_index,
+                box_index);
+
 
             int class_index = entry_index(IH, IW, n*IW*IH + i, 5);
             if(dets[index].objectness){
                 for(int j = 0; j < 80; ++j){
                     int class_index = entry_index(IH, IW, n*IW*IH + i, 4 + 1 + j);
                     float prob = scale*output_data[class_index];
+                    // assert(prob >= 0);
+                    // assert(prob <= 1);
                     dets[index].prob[j] = (prob > thresh) ? prob : 0;
                 }
-                printf("scale:%f [%f, %f]-[%f, %f] score:%f obj_idx:%i box_idx:%i\n", 
-                    scale, 
-                    dets[index].bbox.x, dets[index].bbox.y,
-                    dets[index].bbox.w, dets[index].bbox.h,
-                    dets[index].objectness,
-                    obj_index,
-                    box_index);
+                // printf("scale:%f [%f, %f]-[%f, %f] score:%f obj_idx:%i box_idx:%i\n", 
+                //     scale, 
+                //     dets[index].bbox.x, dets[index].bbox.y,
+                //     dets[index].bbox.w, dets[index].bbox.h,
+                //     dets[index].objectness,
+                //     obj_index,
+                //     box_index);
             }
         }
 
     }
+
+    // for(int index = 0; index < 19*19*5; index++){
+    //     if(dets[index].objectness > 0.4){
+    //         printf("[%f, %f]-[%f, %f] score:%f\n", 
+    //             dets[index].bbox.x, dets[index].bbox.y,
+    //             dets[index].bbox.w, dets[index].bbox.h,
+    //             dets[index].objectness);
+    //     }
+    // }
     correct_region_boxes(dets, IW*IH*5, 1, 1, IW, IH, 0);
 
     return dets;
+}
+
+bool nms_comparator(tools::detection a, tools::detection b)
+{
+    float diff = 0;
+    if(b.sort_class >= 0){
+        diff = a.prob[b.sort_class] - b.prob[b.sort_class];
+    } else {
+        diff = a.objectness - b.objectness;
+    }
+    if(diff < 0) return true;
+    return false;
+}
+
+float overlap(float x1, float w1, float x2, float w2)
+{
+    float l1 = x1 - w1/2;
+    float l2 = x2 - w2/2;
+    float left = l1 > l2 ? l1 : l2;
+    float r1 = x1 + w1/2;
+    float r2 = x2 + w2/2;
+    float right = r1 < r2 ? r1 : r2;
+    return right - left;
+}
+
+float box_intersection(box a, box b)
+{
+    float w = overlap(a.x, a.w, b.x, b.w);
+    float h = overlap(a.y, a.h, b.y, b.h);
+    if(w < 0 || h < 0) return 0;
+    float area = w*h;
+    return area;
+}
+
+float box_union(box a, box b)
+{
+    float i = box_intersection(a, b);
+    float u = a.w*a.h + b.w*b.h - i;
+    return u;
+}
+
+float box_iou(box a, box b)
+{
+    return box_intersection(a, b)/box_union(a, b);
+}
+
+void do_nms_sort(vector<tools::detection>& src_dets, int total, int classes, 
+    float thresh, vector<tools::detection>& dets){
+
+    for(int i = 0; i < src_dets.size(); ++i){
+        if(src_dets[i].objectness){
+            dets.push_back(src_dets.at(i));
+        }
+    }
+
+    for(int k = 0; k < classes; ++k){
+        for(int i = 0; i < dets.size(); ++i){
+            dets[i].sort_class = k;
+        }
+        sort(dets.begin(), dets.end(), nms_comparator);
+        for(int i = 0; i < dets.size(); ++i){
+            if(dets[i].prob[k] == 0) continue;
+            box a = dets[i].bbox;
+            for(int j = i+1; j < dets.size(); ++j){
+                box b = dets[j].bbox;
+                if (box_iou(a, b) > thresh){
+                    dets[j].prob[k] = 0;
+                }
+            }
+        }
+    }
+
+    for(int i = 0; i < dets.size(); i++){
+        printf("dets[%i]: classes: %i sort_class: %i objectness: %f\n", 
+            i, dets[i].classes, dets[i].sort_class, dets[i].objectness);
+    }
+
+}
+
+void draw_detections(cv::Mat im, vector<tools::detection>& dets){
+    
+    for(int i = 0; i < dets.size(); i++){
+        if(dets[i].objectness){
+            box b = dets[i].bbox;
+            int classID = -1;
+            for(int j = 0; j < 80; ++j){
+                if (dets[i].prob[j] > 0.5){
+                    if (classID < 0) {
+                        classID = j;
+                    }
+                    printf("%i: %.0f%%\n", classID, dets[i].prob[j]*100);
+                }
+            }
+            if (classID >= 0){
+                int left  = (b.x-b.w/2.)*im.size().width;
+                int right = (b.x+b.w/2.)*im.size().width;
+                int top   = (b.y-b.h/2.)*im.size().height;
+                int bot   = (b.y+b.h/2.)*im.size().height;
+
+                if(left < 0) left = 0;
+                if(right > im.size().width-1) right = im.size().width-1;
+                if(top < 0) top = 0;
+                if(bot > im.size().height-1) bot = im.size().height-1;
+
+                // printf("Box[%i] [%i, %i] -> [%i, %i]\n", i, left, top, right, bot);
+                cv::Point2d p1 (left, top);
+                cv::Point2d p2 (right, bot);
+                cv::rectangle(im, p1, p2, cv::Scalar(255, 0, 0), 2);
+
+            }
+        }
+    }
+    cv::cvtColor(im, im, cv::COLOR_RGB2BGR);
+    cv::imshow("output", im);
+    cv::waitKey(0);
+
+
 }
 
 }
--- yolo/yolo/main.cpp
+++ yolo/yolo/main.cpp
@@ -60,10 +60,10 @@ int main(int argc, char* argv[]){
         map<string, InputInfo::Ptr>::iterator it = input_info.begin(); 
         it != input_info.end();
         it ++){
-        it->second->setPrecision(Precision::FP32);
+        // it->second->setPrecision(Precision::FP32);
         cout << "Input: " << it->first << endl
             << "\tPrecision: " << it->second->getPrecision() << endl;
-        // it->second->setLayout(Layout::NHWC);
+        it->second->setLayout(Layout::NHWC);
         cout << "\tDim: [ ";
         for(auto x: it->second->getDims()){
             cout << x << " ";
@@ -75,7 +75,7 @@ int main(int argc, char* argv[]){
         map<std::string, DataPtr>::iterator it = output_info.begin();
         it != output_info.end();
         it ++){
-        it->second->setPrecision(Precision::FP32);
+        // it->second->setPrecision(Precision::FP32);
         cout << "Output: " << it->first << endl
             << "\tPrecision: " << it->second->getPrecision() << endl;
         cout << "\tDim: [ ";
@@ -101,41 +101,40 @@ int main(int argc, char* argv[]){
     cv::Mat resizedImg (image);
     cv::imshow("Image", image);
     cv::waitKey(0);
-    cv::cvtColor(image, image, cv::COLOR_BGR2RGB);
     int imw = image.size().width;
     int imh = image.size().height;
-    double resize_ratio = (double)input->getTensorDesc().getDims()[3] / (double)max(imw, imh);
-    image.convertTo(image, CV_32F, 1.0/255.0, 0);
     
     cv::resize(image, resizedImg, cv::Size(input->getTensorDesc().getDims()[3], input->getTensorDesc().getDims()[2]));
+    // cv::cvtColor(resizedImg, resizedImg, cv::COLOR_BGR2RGB);
+    resizedImg.convertTo(resizedImg, CV_32F, 1.0/255.0, 0);
 
     /** Iterating over all input blobs **/
     cout << "Prepare Input: " << input_name << endl;
     /** Getting input blob **/
-    auto data = input->buffer().as<PrecisionTrait<Precision::FP32>::value_type*>();
+    float* data = input->buffer().as<PrecisionTrait<Precision::FP32>::value_type*>();
     /** Setting batch size **/
     network.setBatchSize(1);
 
     /** Iterate over all input images **/
     /** Iterate over all pixel in image (r,g,b) **/
-    // for (size_t ch = 0; ch < num_channels; ch++) {
-    //     /** Iterate over all channels **/
-    //     for (size_t pid = 0; pid < image_size; pid++) {
-    //         /** [images stride + channels stride + pixel id ] all in bytes **/
-    //         // data[ch * image_size + pid] = imageData.get()[pid*num_channels + ch] / 255.0;
-    //         data[ch * image_size + pid] = image.at<float>(ch * image_size + pid);
-    //         // data[ch * image_size + pid] = image.at<float>(pid*num_channels + ch);
-    //     }
-    // }
-
-    for(int ch = 0; ch < 3; ch++){
-        for(int r = 0; r < input->getTensorDesc().getDims()[3]; r++){
-            for(int c = 0; c < input->getTensorDesc().getDims()[2]; c++){
-                data[ch*image_size + r*input->getTensorDesc().getDims()[3] + c] = 
-                    resizedImg.at<cv::Vec3f>(r, c)[ch];
+    for(int k = 0; k < 3; k++){
+        for(int j = 0; j < 608; j++){
+            for(int i = 0; i < 608; i++){
+                // int dst_index = i + 608*j + 608*608*k;
+                int dst_index = i*3 + j*608*3 + k;
+                data[dst_index] = resizedImg.at<cv::Vec3f>(j, i)[k];
             }
         }
     }
+
+    // for(int r = 0; r < 608; r ++){
+    //     for(int c = 0; c < 608; c ++){
+    //         for(int ch = 0; ch < 3; ch ++){
+    //             int dst_index = r*608*3 + c*3 + ch;
+    //             data[dst_index] = resizedImg.at<cv::Vec3f>(r, c)[ch];
+    //         }
+    //     }
+    // }
 
     // 7. Perform Inference
     infer_request.Infer();
@@ -147,15 +146,27 @@ int main(int argc, char* argv[]){
     const Blob::Ptr output_blob = infer_request.GetBlob(output_name);
     float* output_data = output_blob->buffer().as<float*>();
 
+    for(int i = 0; i < 1500; i ++){
+        if(output_data[i] > 1){
+            printf("%i %f\n", i,  output_data[i]);
+        }
+    }
+    cout << endl;
+
     int IH = input->getTensorDesc().getDims()[2];
     int IW = input->getTensorDesc().getDims()[3];
     vector<tools::detection> dets = tools::yoloNetParseOutput(output_data, IH/32, IW/32);
+    vector<tools::detection> do_nms_sort_dets;
+    tools::do_nms_sort(dets, 19*19*5, 80, 0.45, do_nms_sort_dets);
+    tools::draw_detections(resizedImg, do_nms_sort_dets);
 
-    // for(int i = 0; i < dets.size(); i++){
-    //     if(dets[i].objectness > 0.5)
-    //         cout << "det[" << i << "]: " << dets[i] << endl;
+    // for(int index = 0; index < dets.size(); index++){
+    //     if(dets[index].objectness)
+    //     printf("[%f, %f]-[%f, %f] score:%f \n", 
+    //                 dets[index].bbox.x, dets[index].bbox.y,
+    //                 dets[index].bbox.w, dets[index].bbox.h,
+    //                 dets[index].objectness);
     // }
-    // cout << dets[0] << endl;
 
     // for(int i = 0; i < 10; i++){
     //     cout << output_data[i] << " ";
